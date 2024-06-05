1a179d8bccf9b7fa63bd6e4b1ec60a151ef194f7
MirandaFo <120473089+MirandaFo@users.noreply.github.com>
2024-06-05 14:59:34 +0200

    Create Template for all forms 72396.js

---
+++ Template for all forms 72396.js
@@ -0,0 +1,11 @@
+//Retrieve the URL 
+jQuery(document).ready(function() {
+    setTimeout(function() {
+      var parentFrame = window.location;
+      var desiredControl = loader.getEngine().getDocument().getControlsList().find(function(control) { 
+        return control.id === 2912198; // the desired control id
+      });
+      desiredControl.setValue({value: parentFrame.href});
+      console.log('custom static resource loaded');
+    }, 2000);
+  });
