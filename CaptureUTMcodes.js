4c38bc09616f709a5d55bb145152a17f13e9ace6
MirandaFo <120473089+MirandaFo@users.noreply.github.com>
2024-06-10 11:15:43 +0200

    Update CaptureUTMcodes.js

--- CaptureUTMcodes.js
+++ CaptureUTMcodes.js
@@ -8,10 +8,10 @@ var utmSource = urlParams.get('utm_campaign');
 var utmMedium = urlParams.get('utm_source');
 var utmCampaign = urlParams.get('utm_medium');
 
-loader.engine.document.getElementById(111111111).setValue({ value: utmSource });
-loader.engine.document.getElementById(222222222).setValue({ value: utmMedium });
-loader.engine.document.getElementById(333333333).setValue({ value: utmCampaign });
-loader.engine.document.getElementById(444444444).setValue({ value: parentFrame });
+loader.engine.document.getElementById(2925815).setValue({ value: utmSource });
+loader.engine.document.getElementById(2925816).setValue({ value: utmMedium });
+loader.engine.document.getElementById(2925814).setValue({ value: utmCampaign });
+loader.engine.document.getElementById(2912198).setValue({ value: parentFrame });
 
 }, 1000);}
 window.onclick = getReferrer;
